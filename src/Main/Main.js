b30548cda379921783abfcc338a9f24d0b005f2d
Gauthierlaplace <gauthier.laplace@gmail.com>
2023-06-09 14:53:23 +0200

    fix conflicts

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,24 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
 import './Main.scss';
-
 import Home from '../Home/Home';
 import Game from '../Game/Game';
-
 //TODO fix la gestion des Routes
 function Main() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<Home />}
-      />
-    
-      <Route
-        path="/game"
-        element={<Game />}
-      />
-    </Routes>
+    <div className="Main">
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/game"
+          element={<Game />}
+        />
+      </Routes>
+    </div>
   );
 }
-
 export default Main;
