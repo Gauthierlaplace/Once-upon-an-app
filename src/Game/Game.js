43f693622229e991f80e6540108f97d0c8cb748a
Sandra-Allies <sandra.allies.fr@gmail.com>
2023-06-11 16:19:30 +0200

    css edit for Log, scrollbar OK, messages with margin-bottom OK, text log with margin for P and line-height OK, change h1 'journal' from log => in Game.js

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -4,14 +4,27 @@ import PlayerHealth from './PlayerHealth/PlayerHealth';
 import Scene from './Scene/Scene';
 import Log from './Log/Log';
 import Menus from './Menus/Menus';
+// import NPC from './NPC/NPC';
+
 
 function Game() {
   return (
     <div className="Game">
       <PlayerHealth />
-      <Scene />
-      <Log />
-      <Menus />
+
+      <div className="Game-flexSB">
+        <div className="Game-left">
+          <Scene />
+          {/* <NPC /> */}
+          <Menus />
+        </div>
+
+        <div className="Game-right">
+        <h1 className="Log-title">Journal</h1>
+          <Log />
+        </div>
+      </div>
+
     </div>
   );
 }
--- src/Game/Scene/Scene.js
+++ src/Game/Scene/Scene.js
@@ -1,4 +1,3 @@
-import NPC from '../NPC/NPC';
 import './Scene.scss';
 
 function Scene() {
@@ -6,7 +5,6 @@ function Scene() {
     <div className="Scene">
         <h1 className="Scene-title">Event Title</h1>
         <img className="Scene-img" src='https://cdn.midjourney.com/7f09becc-bec9-4b15-a9ec-a476bbbeb5aa/0_1.png' alt='scene' />
-        <NPC />
     </div>
   );
 }
