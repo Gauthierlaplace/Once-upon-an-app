0bf0cc67e7a2606822812e915b75db5cc296be7e
Gauthierlaplace <gauthier.laplace@gmail.com>
2023-06-16 11:51:29 +0200

    reset and postponed, with formik and yuk

---
+++ src/components/Account/Account.js
@@ -0,0 +1,11 @@
+import './Account.scss';
+
+function Account() {
+  return (
+    <div className="Account">
+      {/* // Todo installer formik et yuk */}
+    </div>
+  );
+}
+
+export default Account;
