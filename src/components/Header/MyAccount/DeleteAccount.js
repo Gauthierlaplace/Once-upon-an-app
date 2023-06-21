ddbd9d6ba1df5d1ef36e2d4c54a4f07ded38e000
Sandra-Allies <sandra.allies.fr@gmail.com>
2023-06-21 23:27:52 +0200

    create component ShowAccount, EditAccount and DeleteAccount. useState for show or hidden component

---
+++ src/components/Header/MyAccount/DeleteAccount.js
@@ -0,0 +1,9 @@
+import './MyAccount.scss';
+
+function DeleteAccount() {
+  return (
+    <h1>Delete Account</h1>
+  );
+}
+
+export default DeleteAccount;
--- src/components/Header/MyAccount/MyAccount.js
+++ src/components/Header/MyAccount/MyAccount.js
@@ -1,9 +1,73 @@
 import './MyAccount.scss';
+import { useState } from 'react';
+import ShowAccount from './ShowAccount';
+import EditAccount from './EditAccount';
+import DeleteAccount from './DeleteAccount';
 
 function MyAccount() {
+  // VARIABLE ET FONCTION POUR AFFICHER LE BON COMPONENT
+  // LORS DU ONCLICK
+  const [displayAccount, setDisplayAccount] = useState(true);
+  const [displayEdit, setDisplayEdit] = useState(false);
+  const [displayDelete, setDisplayDelete] = useState(false);
+
+  const displayAccountFunction = () => {
+    setDisplayAccount(true);
+    setDisplayEdit(false);
+    setDisplayDelete(false);
+  };
+
+  const displayEditFunction = () => {
+    setDisplayAccount(false);
+    setDisplayEdit(true);
+    setDisplayDelete(false);
+  };
+
+  const displayDeleteFunction = () => {
+    setDisplayAccount(false);
+    setDisplayEdit(false);
+    setDisplayDelete(true);
+  };
+
   return (
     <div className="MyAccount">
-      <h1>Je suis la fonction MyAccount</h1>
+
+      <div className="MyAccount-container">
+
+        <div className="MyAccount-title">
+          <h1>Bienvenue sur votre compte</h1>
+        </div>
+
+        <div className="MyAccount-main">
+
+          <div className="MyAccount-left">
+            <nav>
+              <ul>
+                <li onClick={displayAccountFunction}>Mon compte</li>
+                <li onClick={displayEditFunction}>Modifier mon compte</li>
+                <li onClick={displayDeleteFunction}>Supprimer mon compte</li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="MyAccount-right">
+
+            {displayAccount && (
+              <ShowAccount />
+            )}
+
+            {displayEdit && (
+              <EditAccount />
+            )}
+
+            {displayDelete && (
+              <DeleteAccount />
+            )}
+          </div>
+
+        </div>
+
+      </div>
     </div>
   );
 }
