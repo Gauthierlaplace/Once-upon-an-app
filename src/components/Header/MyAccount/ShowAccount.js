272b27aada996c2bffdec64faef1891fa242f11c
Sandra-Allies <sandra.allies.fr@gmail.com>
2023-06-26 07:32:47 +0200

    commit for check other branch if merge or not

--- src/components/Header/MyAccount/ShowAccount.js
+++ src/components/Header/MyAccount/ShowAccount.js
@@ -1,8 +1,24 @@
 import './MyAccount.scss';
+import { useSelector } from 'react-redux';
+// import api from '../'
 
 function ShowAccount() {
+  const nickname = useSelector((state) => state.user.nickname);
+  const heroName = useSelector((state) => state.game.heroData[0].name);
+  const heroPicture = useSelector((state) => state.game.heroData[0].picture);
+  const emailLogin = useSelector((state) => state.user.email);
+
+// api
+// .get(
+//   '/users'
+// )
   return (
-    <h1>Show Account</h1>
+    <div className="Show">
+      <h1>{heroName}</h1>
+      <h2>{nickname}</h2>
+      {/* <h2>{emailLogin}</h2> */}
+      {/* <img src={heroPicture} alt="Avatar Hero" /> */}
+    </div>
   );
 }
 
