7054d3d338067588173d96a4ff5dc7a556c14c12
marine-spaak <marine.spaak@gmail.com>
2023-06-26 11:04:05 +0200

    first functionnality of delete working without dispatch

--- src/components/Header/MyAccount/DeleteAccount.js
+++ src/components/Header/MyAccount/DeleteAccount.js
@@ -1,8 +1,48 @@
 import './MyAccount.scss';
+import { useSelector } from 'react-redux';
+import api from '../../../api/api';
 
+// ======================================
+// FONCTION POUR LA SUPPRESSION DU COMPTE
+// ======================================
+
+const deleteUserAccount = (userId) => {
+  api
+    .delete(`/users/${userId}`)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+// ======================================
+// FIN DE LA FONCTION DE DELETE
+// ======================================
 function DeleteAccount() {
+  // const : tuserId utilisé pour la fonction delete account
+  const userId = useSelector((state) => state.user.currentUserId);
+
   return (
-    <h1>Delete Account</h1>
+    <div className="DeleteAccount">
+      <h1>Supprimer mon compte</h1>
+      <p>Attention, en cliquant sur le bouton "supprimer mon compte" ci dessous,
+        cela entrainement la supprésszion définitive de votre compte et vous perdrez
+        toute votre progression dans le jeu.
+      </p>
+      <p>
+        Si vous souhaitez rejouer à nouveau, il vous faudra créer un nouveau compte.
+      </p>
+
+      <h1>Souhaitez-vous supprimer votre compte ?</h1>
+      <button
+        type="button"
+        onClick={() => deleteUserAccount(userId)}
+      >
+        Oui je souhaite supprimer mon compte
+      </button>
+    </div>
   );
 }
 
